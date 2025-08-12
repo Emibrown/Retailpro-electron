41c90a1ab45c745b9d583f5190388c7b9f016a8a
Emibrown <emekaekene@gmail.com>
2025-08-12 05:01:53 +0100

    chore: bootstrap RetailPro Electron app

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,10 @@
+## Summary
+
+- What changed? Why?
+
+## Testing
+- [ ] Built locally on my OS
+- [ ] Smoke-tested add-to-cart, tabs, and quantity controls
+
+## Screenshots
+(attach if UI changed)
---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  win.setMenuBarVisibility(false);
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
